d34c386bd203f98a20ea55eb16daae5d2d63fb00
nbogie <neillbogie@googlemail.com>
2021-01-31 19:14:48 +0000

    Alternate refactoring
    No globals, all data passed / returned
    Take guess in only one place
    No use of * operator
    Removed timings, countdown

--- refactored/quiz.py
+++ refactored/quiz.py
@@ -1,170 +1,169 @@
 import random
-import time
-
-lives = 3
-score = 0
-t = 3
 
 dashes = ("\n---------------------------------------------------")
 
-categories = {
-  "CONTINENT" : ["EUROPE", "ASIA", "AFRICA", "NORTH AMERICA", "SOUTH AMERICA", "ANTARTICA", "OCEANIA"],
-  "COLOUR" : ["RED", "BLUE", "GREEN", "PINK", "PURPLE"],
-  "SPORTS" : ["TENNIS", "BASKETBALL", "FOOTBALL", "VOLLEYBALL", "HOCKEY"],
-  "UK CITY": ["LONDON", "BIRMINGHAM", "MANCHESTER", "LIVERPOOL", "YORK"],
-  "CODING LANGUAGES": ["PYTHON", "JAVASCRIPT", "JAVA", "TYPESCRIPT", "RUBY", "C"]
-  }
 
-# This function removes the correct answer from the categories dictionary
-def remove_answer(category, word_to_guess):
-  categories[category].remove(word_to_guess)
-  print(len(categories[category]), " rounds remaining.")
+def remove_answer_from_category(answer, category):
+    if answer in category:
+        category.remove(answer)
 
-# This function counts down time from 3 seconds and then clears the jumble word
-def countdown(t, jumbled_word): 
-  while t != -1:
-    mins, secs = divmod(t, 60) # the divmod function takes two numbers and returns a pair of numbers (a tuple) consisting of their quotient and remainder
-    timer = '{:02d}:{:02d}'.format(mins, secs)
-    print(*jumbled_word," ", timer, sep='', end="\r") 
-    #the * brings it out of the list, then sep removes the spaces/commas
-    time.sleep(1) 
-    t -= 1
 
-# Increases score and prints new score
-def score_increase():
-  global score
-  score += 1
-  print("Your score is: ", score)
+# Randomly selects a word from the chosen category and jumbles its letters.
+# Returns a tuple of two strings: (answer, jumbled answer)
 
-# Randomly chooses a word from the chosen category
-def get_word(category):
-  return random.choice(categories[category])
 
-# Shuffles the letters in a word
-def jumble_word(word):
-  to_jumble = list(word)
-  random.shuffle(to_jumble)
-  return to_jumble
+def pick_random_question(category):
+    answer = random.choice(category)
+    remove_answer_from_category(answer, category)
 
-# Asks you if you want a clue and gives you a clue if so
-def give_clue(word_to_guess):
-  clue_input = input("Wrong answer!!! \nWould you like a Clue, yes/no: ").lower()
-  wants_clue = (clue_input == "yes" or clue_input == "y")
-  if wants_clue:
-    print(f"The first letter of the word is {word_to_guess[0]}")
-  return wants_clue
+    jumbled_letters = list(answer)
+    random.shuffle(jumbled_letters)
+    jumble = "".join(jumbled_letters)
+    return answer, jumble
 
-# Asks user to guess again
-def get_later_guess(word_to_guess, wants_clue):
-  new_guess = input("Guess again: ").upper()
-  while new_guess != word_to_guess:
-    lose_life()
-    if wants_clue:
-      new_guess = input(f"Even with a clue, LOL...guess again: ").upper()
+
+# Asks you if you want a clue and gives you a clue
+# Returns True if you requested a clue, else False
+
+
+def offer_and_give_clue(answer):
+    clue_choice = input("Would you like a clue, yes/no: ").lower()
+    if clue_choice == "yes" or clue_choice == "y":
+        print(f"The first letter of the word is {answer[0]}")
+        return True
     else:
-      new_guess = input(f"Guess again: ").upper()
-  return new_guess
-
-# Removes a life and checks if ran out of lives
-def lose_life():
-  global lives
-  lives -= 1
-  # TODO: make the word life dynamic - eg 1 life, 2 lives ...
-  print(f"You got this wrong! You have {lives} lives remaining")
-  if lives == 0:
-    print("\n No juice left in the tank \n\n!!!!!!GAME OVER!!!!!!")
-    time.sleep(1)
-    print(f"Your final score is {score}.\n\n\n")
-    exit()
+        return False
 
 
-# Asks user to pick category
-def choose_category():
-  print("\nSelect a category out of:")
-  for key in categories:
-    print(key.title())
-  print(dashes)
-  user_input = input("Category Choice: ").upper()
-  while user_input not in categories:
-    user_input = input("Select from list above: ").upper()
-  return user_input
+# Asks the player to select a category
+# Returns tuple: the chosen category name, its list of questions
 
-# Prints welcome and instructions
-def print_welcome():
-  print (
-    """             
+
+def get_category_choice(categories):
+    print("\nSelect a category out of:")
+    for key in categories:
+        print(key.title())
+    print(dashes)
+    category_name = input("Category Choice: ").upper()
+    while category_name not in categories:
+        category_name = input("Select from list above: ").upper()
+
+    category = categories[category_name]
+    del categories[category_name]
+
+    return category, category_name
+
+
+def display_intro():
+    print("""
     Welcome to the Academy Anagram Game!
     👋 """)
 
-  print (
-    """ 
+    print(
+        """
     HOW TO COMPLETE THE GAME:
     - Choose a category.
     - Unscramble the jumbled word and type out your guess.
     - If you get it wrong, not to worry, you can get a clue!
     - Guess all the words from each category!
-    - However, be careful not to get too many wrong! 
+    - However, be careful not to get too many wrong!
     - NOTE: YOU ONLY HAVE 3 LIVES!👀
     \n
-    INCREASED DIFFICULTY:
-    - Difficulty increases when you transition to the next category
-    - You have 3 seconds to look at the jumbled word
-    - Then it will disappear and you will then be able to guess
-  """)
+    """)
 
-# Prints jumbled word (and hides it if difficulty higher)
-def give_starting_point(difficulty, jumbled_word):
-  if difficulty >= 1:
-      countdown(t, jumbled_word)
-      print("*******POOOOF*******")
-  else:
-      print (*jumbled_word, sep='') 
-      #the * brings it out of the list, then sep removes the spaces/commas
 
-# Calls other functions to hadnle correct answer
-def handle_right_answer(category, word_to_guess):
-  print ("Got It!")
-  time.sleep(0.5)
-  remove_answer(category, word_to_guess) 
-  score_increase()
-  time.sleep(0.5)
+def display_question(category_name, jumble):
+    print(dashes)
+    print("What " + category_name.title() + " is this?")
+    print(jumble)
 
-# Checks if guess is correct and calls functions for each option
-def handle_guess(guess, word_to_guess, category):
-  if guess == word_to_guess:
-      handle_right_answer(category,word_to_guess)
-  else:
-      lose_life()
-      wants_clue = give_clue(word_to_guess)
-      next_guess = get_later_guess(word_to_guess, wants_clue)
-      if next_guess == word_to_guess:
-        handle_right_answer(category, word_to_guess)
+# Return the user's guess
 
-# Governs the play of a single category
-def play_category(category, difficulty):
-  while len(categories[category]) > 0 and lives > 0:
-      word_to_guess = get_word(category)
-      jumbled_word = jumble_word(word_to_guess)
-      print (dashes)
-      print("What " + category.title() + " is this?")
-      give_starting_point(difficulty,jumbled_word)
-      guess = input("Your guess: ").upper()
-      handle_guess(guess, word_to_guess, category)
-  
-# Overall game
-def play_game():
-  difficulty = 0
-  print_welcome()
 
-  while len(categories) > 0 and lives > 0:
-    category = choose_category()
-    play_category(category, difficulty)
+def prompt_and_get_guess():
+    return input("Guess: ").upper()
 
-    del categories[category]
-    difficulty += 1
 
-  print("\nCONGRATULATIONS, you've reached the end of the game!")
-  print(f"Your final score is {score} and you had {lives} lives remaining.\n\n\n")
+def say_wrong(has_used_clue, lives):
+    if has_used_clue:
+        print("Wrong!  Even with a clue, LOL...")
+    else:
+        print("Wrong!")
+    print("Number of lives remaining: ", lives)
 
-# Executes the main game function
-play_game()
+
+# Handle user guessing (and clue-giving) for the given question
+# Returns number of lives remaining after guesses (possibly zero)
+
+
+def handle_guesses(answer, lives):
+    guessed_correctly = False
+    has_used_clue = False
+
+    while not guessed_correctly and lives > 0:
+        guess = prompt_and_get_guess()
+        if guess == answer:
+            guessed_correctly = True
+            print("Right!")
+        else:
+            lives -= 1
+            say_wrong(has_used_clue, lives)
+            if (lives > 0 and not has_used_clue):
+                has_used_clue = offer_and_give_clue(answer)
+
+    return lives
+
+
+def create_categories():
+    return {
+        "CONTINENT": ["EUROPE", "ASIA", "AFRICA", "NORTH AMERICA", "SOUTH AMERICA", "ANTARTICA", "OCEANIA"],
+        "COLOUR": ["RED", "BLUE", "GREEN", "PINK", "PURPLE"],
+        "SPORT": ["TENNIS", "BASKETBALL", "FOOTBALL", "VOLLEYBALL", "HOCKEY"],
+        "UK CITY": ["LONDON", "BIRMINGHAM", "MANCHESTER", "LIVERPOOL", "YORK"],
+        "CODING LANGUAGE": ["PYTHON", "JAVASCRIPT", "JAVA", "TYPESCRIPT", "RUBY", "C"]
+    }
+
+
+# Display an update of score and number of rounds remaining in the current category
+
+
+def display_update(score, lives, category, category_name):
+    print("Your score is: ", score, "and you have", lives, "lives remaining.")
+    rounds_remaining = len(category)
+    if rounds_remaining > 0:
+        print(len(category), "rounds remain in the category", category_name)
+    else:
+        print("That's the end of category", category_name)
+
+
+# Play the entire game
+
+
+def play():
+    lives = 3
+    score = 0
+    categories = create_categories()
+
+    display_intro()
+
+    while len(categories) > 0 and lives > 0:
+        (category, category_name) = get_category_choice(categories)
+
+        while len(category) > 0 and lives > 0:
+            (answer, jumble) = pick_random_question(category)
+            display_question(category_name, jumble)
+            lives = handle_guesses(answer, lives)
+
+            if lives > 0:
+                score += 1
+                display_update(score, lives, category, category_name)
+
+    # At the end, either because lives exhausted or because all categories were completed.
+    if (lives > 0):
+        print("\nCONGRATULATIONS, you've reached the end of the game!")
+    else:
+        print("\nNo juice left in the tank\n\n!!!!!!GAME OVER!!!!!!")
+    print(f"Your final score is {score} and you had {lives} lives remaining.\n\n\n")
+
+
+play()
