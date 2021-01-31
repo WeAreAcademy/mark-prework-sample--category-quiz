5ce3abdf8ba6847af101c304e1c7cf6d3f35c124
Esme Hotston Moore <ehotstonmoore@hotmail.co.uk>
2021-01-31 14:52:21 +0000

    making word_to_guess and jumbled_word variables not global

--- refactored/quiz.py
+++ refactored/quiz.py
@@ -18,20 +18,17 @@ categories = {
   }
 
 # This function removes the correct answer from the dictionary
-def remove_answer(category):
-  global answer # global keyword allows you to modify/use the variable outside of the current scope
-  val_to_del = answer
-  for key in categories[category]: 
-    if val_to_del in categories[category]:
-      categories[category].remove(val_to_del)
+def remove_answer(category, word_to_guess):
+  val_to_del = word_to_guess
+  categories[category].remove(val_to_del)
   print(len(categories[category]), " rounds remaining.")
 
 # This function counts down time from 3 seconds and then clears the jumble word
-def countdown(t): 
+def countdown(t, jumbled_word): 
   while t != -1:
     mins, secs = divmod(t, 60) # the divmod function takes two numbers and returns a pair of numbers (a tuple) consisting of their quotient and remainder
     timer = '{:02d}:{:02d}'.format(mins, secs)
-    print(*jumble," ", timer, sep='', end="\r") 
+    print(*jumbled_word," ", timer, sep='', end="\r") 
     #the * brings it out of the list, then sep removes the spaces/commas
     time.sleep(1) 
     t -= 1
@@ -43,26 +40,34 @@ def score_increase():
   print("Your score is: ", score)
   
 # This function randomly selects a word from the chosen category and jumbles it
-def random_selection(category):
-  selection = random.choice(categories[category])
-  global answer
-  answer = selection
-  global jumble
-  jumble = list(selection)
-  random.shuffle(jumble)
+# def random_selection(category):
+#   selection = random.choice(categories[category])
+#   global answer
+#   answer = selection
+#   global jumble
+#   jumble = list(selection)
+#   random.shuffle(jumble)
+
+def get_word(category):
+  return random.choice(categories[category])
+
+def jumble(word):
+  to_jumble = list(word)
+  random.shuffle(to_jumble)
+  return to_jumble
 
 # This function asks you if you want a clue and gives you a clue
-def clue_func():
+def clue_func(word_to_guess):
   global clue_choice
   clue_choice = input("Wrong answer!!! \nWould you like a Clue, yes/no: ").lower()
   if clue_choice == "yes" or clue_choice == "y":
-    global answer
-    print(f"The first letter of the word is {answer[0]}")
+    
+    print(f"The first letter of the word is {word_to_guess[0]}")
 
 # This function asks user to guess again if answer is wrong
-def new_guess():
+def new_guess(word_to_guess):
   new_guess = input("Guess again: ").upper()
-  while new_guess != answer:
+  while new_guess != word_to_guess:
     life_wrong()
     global clue_choice
     if clue_choice == "yes" or clue_choice == "y":
@@ -123,31 +128,33 @@ def play_game():
   while len(categories) > 0 and lives > 0:
     category = choose_category()
     while len(categories[category]) > 0 and lives > 0:
-      random_selection(category)
+      # random_selection(category)
+      word_to_guess = get_word(category)
+      jumbled_word = jumble(word_to_guess)
       print (dashes)
       print("What " + category.title() + " is this?")
       global difficulty
       if difficulty >= 1:
-        countdown(t)
+        countdown(t, jumbled_word)
         print("*******POOOOF*******")
       else:
-        print (*jumble, sep='') 
+        print (*jumbled_word, sep='') 
         #the * brings it out of the list, then sep removes the spaces/commas
       guess = input("Answer: ")
       guess = guess.upper()
 
-      if guess == answer:
+      if guess == word_to_guess:
         print ("Got It!")
         #then remove this answer from the list
         time.sleep(0.5)
-        remove_answer(category) 
+        remove_answer(category, word_to_guess) 
         score_increase()
         time.sleep(0.5)
       else:
         life_wrong()
-        clue_func()
-        new_guess()
-        remove_answer(category)
+        clue_func(word_to_guess)
+        new_guess(word_to_guess)
+        remove_answer(category, word_to_guess)
         score_increase()
 
     del categories[category]
